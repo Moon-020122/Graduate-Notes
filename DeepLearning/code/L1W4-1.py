49fa75101371541faa22e6a8b9fb715213fa10f8
Moon-020122 <1262570172@qq.com>
2024-06-01 11:05:49 +0800

    2024-6-1

---
+++ DeepLearning/code/L1W4-1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from lib.testCases_v2 import *
+from lib.dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+
+np.random.seed(1)
